722feff1b9348c4d4f3ad707f43415da87ca84a0
Chris Gavin <chris@chrisgavin.me>
2021-12-08 11:27:06 +0000

    Add the initial code.

---
+++ src/java.java
@@ -0,0 +1,15 @@
+public class java {
+	public static void main(String[] args) {
+		int[] ints = new int[1];
+		ints[0] = 0;
+		for (int i=0; i < ints.length; i++) {
+			int x = ints[i+1]; // true
+			int y = ints[i+1]; // lgtm[java/index-out-of-bounds] false
+			@SuppressWarnings("lgtm[java/index-out-of-bounds]")
+			int z = ints[i+1]; // false
+			System.out.println(x);
+			System.out.println(y);
+			System.out.println(z);
+		}
+	}
+}
